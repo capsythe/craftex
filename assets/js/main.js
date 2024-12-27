3684e4fa559143e88677479169e3e09f8d3634b9
Blythe <blythe@exprezoe.com>
2024-12-27 09:07:44 +0700

    content changed

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,11 +57,11 @@ const sendEmail = (e) => {
 contactForm.addEventListener('submit', sendEmail)
 
 const scrollUp = () => {
-    const scrollUp = document.getElementById('scroll-up')
+    const scrollUp = document.getElementById('scroll-up');
     // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scroll-top class
-    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll')
+    window.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll');
 }
-window.addEventListener('scroll', scrollUp)
+window.addEventListener('scroll', scrollUp);
 
 const sections = document.querySelectorAll('section[id]')
 
